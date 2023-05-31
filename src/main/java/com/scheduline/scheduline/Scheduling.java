7c6ee7b0c7a0e8e2f3a77e477f9303b789849375
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 11:58:33 +0400

    the 11 quation is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "0 0 1 * 1 *")
+    @Scheduled(cron = "0 0,30 8-17 * * 5-6")
     public void SchedulerTask()
     {
-        System.out.println("runs a job every hour during the month of January.");
+        System.out.println("runs a job every 30 minutes during business hours (8:00 AM to 5:00 PM) on weekdays.");
     }
 
 }
