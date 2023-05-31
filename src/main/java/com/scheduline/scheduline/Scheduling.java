7fa60c061f1e531e0852e63836e631b9195fd68a
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 12:28:16 +0400

    the quation 20 is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "* * 0 0+2 */\n")
+    @Scheduled(cron = "* 0/5 9-18 * * 5-6")
     public void SchedulerTask()
     {
-        System.out.println("runs a job every hour but only on even-numbered days of the month.");
+        System.out.println("runs a job every 5 minutes but only between 9:00 AM and 6:00 PM on weekends");
     }
 
 }
