6f4eb897d19be7aca19362be8b08d61dba87a4a7
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 12:20:46 +0400

    the quation 17 is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "* 0 * 1-15 * *")
+    @Scheduled(cron = "* */5 * * * 0-4" +
+                      "* */15 * * * 5-6")
     public void SchedulerTask()
     {
-        System.out.println("runs a job every hour during the first half of the month");
+        System.out.println("runs a job every 5 minutes on weekdays and every 15 minutes on weekends.");
     }
 
 }
