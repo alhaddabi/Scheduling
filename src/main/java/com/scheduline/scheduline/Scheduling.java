a5fa06073ef863a4692f73b3c84280b76cf29c75
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 14:55:38 +0400

    the quation 38 is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "* */15 14-22 * * 0-4")
+    @Scheduled(cron = "* */10 8-14 * * 0-4")
     public void SchedulerTask()
     {
-        System.out.println("runs a job every 15 minutes from 2:00 PM to 10:00 PM on weekends");
+        System.out.println("runs a job every 10 minutes on weekdays between 8:00 AM and 3:00 PM.");
     }
 
 }
