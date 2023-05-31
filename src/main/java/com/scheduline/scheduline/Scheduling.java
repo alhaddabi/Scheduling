c130e8093100b470ccbdb52c8bf2c57dee942b43
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 14:44:32 +0400

    the quation 31 is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "* 0 12 * 7 1-6")
+    @Scheduled(cron = "* 30 23 * * 6")
     public void SchedulerTask()
     {
-        System.out.println("runs a job at 12:00 PM every day except Sunday in the month of July");
+        System.out.println(" runs a job at 11:30 PM every Friday.");
     }
 
 }
