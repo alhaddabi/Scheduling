65bce2bb58737302d04886948e5bfa9d3e0e8380
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 14:27:28 +0400

    the quation 22 is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "* 30 3 * * * 1-5")
+    @Scheduled(cron = "* 0 9-17 15 * *")
     public void SchedulerTask()
     {
-        System.out.println(" runs a job at 3:30 AM every day except Saturday and Sunday.");
+        System.out.println(" runs a job at the top of every hour between 9:00 AM and 5:00 PM on the 15th day of the month");
     }
 
 }
