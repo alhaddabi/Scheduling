4f9a0e56dfa5f2d5061f6c976a9ac35cde54953e
alhaddbi <adnan.alhddabi.5@gmail.com>
2023-05-31 11:49:08 +0400

    the 5 quation is solved

--- src/main/java/com/scheduline/scheduline/Scheduling.java
+++ src/main/java/com/scheduline/scheduline/Scheduling.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduling {
-    @Scheduled(cron = "* * 8 * * 2")
+    @Scheduled(cron = "0 0 3 1 * *")
     public void SchedulerTask()
     {
-        System.out.println("runs a job every Monday at 8:00 AM.");
+        System.out.println("runs a job every month on the first day of the month at 3:00 AM.");
     }
 
 }
